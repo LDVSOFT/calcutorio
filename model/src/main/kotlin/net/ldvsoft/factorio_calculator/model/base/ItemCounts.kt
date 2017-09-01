f333c40d0d5f60c4a603395192e7361fef5e0ffe
Lapshin Dmitry (LDVSOFT) <lapshin.dv@mail.ru>
2017-09-02 01:50:56 +0300

    Added ItemCounts tests; fixed code to pass them

--- model/src/main/kotlin/net/ldvsoft/factorio_calculator/model/base/ItemCounts.kt
+++ model/src/main/kotlin/net/ldvsoft/factorio_calculator/model/base/ItemCounts.kt
@@ -3,7 +3,7 @@ package net.ldvsoft.factorio_calculator.model.base
 infix fun Int.of(item: Item) = item to toDouble()
 infix fun Double.of(item: Item) = item to this
 
-class ItemCounts(val counts: Map<Item, Double> = emptyMap()): Map<Item, Double> by counts {
+data class ItemCounts(val counts: Map<Item, Double> = emptyMap()): Map<Item, Double> by counts {
     constructor(vararg counts: Pair<Item, Double>): this(mapOf(*counts))
 
     override operator fun get(key: Item): Double {
@@ -12,13 +12,13 @@ class ItemCounts(val counts: Map<Item, Double> = emptyMap()): Map<Item, Double> 
 
     operator fun plus(that: ItemCounts): ItemCounts {
         val resultMap = counts.toMutableMap()
-        that.forEach { item, d -> resultMap[item] = resultMap.getOrDefault(item, .0) + d }
+        that.forEach { (item, d) -> resultMap[item] = resultMap.getOrDefault(item, .0) + d }
         return ItemCounts(resultMap)
     }
 
     operator fun minus(that: ItemCounts): ItemCounts {
         val resultMap = counts.toMutableMap()
-        that.forEach { item, d -> resultMap[item] = resultMap.getOrDefault(item, .0) - d }
+        that.forEach { (item, d) -> resultMap[item] = resultMap.getOrDefault(item, .0) - d }
         return ItemCounts(resultMap)
     }
 
@@ -31,6 +31,8 @@ class ItemCounts(val counts: Map<Item, Double> = emptyMap()): Map<Item, Double> 
     }
 
     operator fun div(k: Double): ItemCounts {
+        if (k == 0.0)
+            throw IllegalArgumentException("Division by zero")
         return times(1 / k)
     }
 
@@ -38,18 +40,12 @@ class ItemCounts(val counts: Map<Item, Double> = emptyMap()): Map<Item, Double> 
         return that.map { (item, cnt) -> this[item] / cnt }.min() ?: Double.POSITIVE_INFINITY
     }
 
-    fun notGreaterThan(that: ItemCounts) = all { (item, value) -> value <= that[item] }
-
-    inline fun forEach(body: (Item, Double) -> Unit) {
-        counts.entries.forEach { (item, cnt) -> body(item, cnt)}
-    }
-
     fun filerNonZero(): ItemCounts {
         return ItemCounts(counts.filterValues { it != .0 })
     }
 
     fun ceilWith(ceil: ItemCounts): ItemCounts {
-        return ItemCounts(ceil.mapValues { (item, cnt) -> maxOf(cnt, this[item]) })
+        return ItemCounts(mapValues { (item, cnt) -> minOf(cnt, ceil[item]) })
     }
 
     operator fun contains(item: Item) = keys.contains(item)
