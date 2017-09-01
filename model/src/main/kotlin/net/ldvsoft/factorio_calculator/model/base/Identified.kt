6bfb8a158d2c219706660e94c5a92e0cdeebb5c1
Lapshin Dmitry (LDVSOFT) <lapshin.dv@mail.ru>
2017-09-02 00:58:49 +0300

    Made all Identified sortable

--- model/src/main/kotlin/net/ldvsoft/factorio_calculator/model/base/Identified.kt
+++ model/src/main/kotlin/net/ldvsoft/factorio_calculator/model/base/Identified.kt
@@ -1,8 +1,14 @@
 package net.ldvsoft.factorio_calculator.model.base
 
-interface Identified {
+interface Identified: Comparable<Identified> {
     val id: String
     val idType: String
+
+    override fun compareTo(other: Identified): Int {
+        if (idType != other.idType)
+            throw IllegalArgumentException("Cannot compare Identified of different type")
+        return id.compareTo(other.id)
+    }
 }
 
 abstract class IdentifiedObject(override val idType: String): Identified
